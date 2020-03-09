bee5562862dcb0104b27848f484eb4441c762ff8
ggmartins <ggmartins@gmail.com>
2020-03-09 00:14:06 -0500

    feat: iotmon json post

--- iotmon/src/iotmon.go
+++ iotmon/src/iotmon.go
@@ -5,22 +5,43 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
+	"reflect"
 	"regexp"
 	"strings"
+	"time"
+
+	"github.com/google/uuid"
+	"gopkg.in/yaml.v3"
 )
 
-type iotBundle struct {
-	id               string
-	idType           string
-	bundleList       []string
-	bundleListType   []string
-	bundleListResolv []string
+//Config monitor config
+type Config struct {
+	URL string `yaml:"url"`
 }
 
+//IoTBundle status update, do not change
+type IoTBundle struct {
+	UUID             string   `yaml:"uuid" json:"uuid"`
+	ID               string   `yaml:"id" json:"id"`
+	IDType           string   `yaml:"idType" json:"idType"`
+	BundleList       []string `yaml:"bundleList,flow" json:"bundleList,flow"`
+	BundleListType   []string `yaml:"bundleListType,flow" json:"bundleListType,flow"`
+	BundleListResolv []string `yaml:"bundleListResolv,flow" json:"bundleListResolv,flow"`
+	BundleListDesc   []string `yaml:"bundleListDesc,flow" json:"bundleListDesc,flow"`
+	BundleListUUID   []string `yaml:"bundleListUUID,flow" json:"bundleListUUID,flow"`
+	BundleListAdd    []string `yaml:"-" json:"bundleListAdd`
+	BundleListDel    []string `yaml:"-" json:"bundleListDel`
+	HasChanged       bool     `yaml:"-" json:"hasChanged`
+	LastSeen         string   `yaml:"-" json:"lastSeen`
+}
+
+const iotstatusyaml = "./iotmon.status.yaml"
+const iotconfigyaml = "./iotmon.config.yaml"
 const ouiFileURL = "http://standards-oui.ieee.org/oui.txt"
 const ouiFileLocal = "/tmp/oui.txt"
 
@@ -93,7 +114,7 @@ func minimum(a, b, c int) int {
 	return c
 }
 
-func getStatusUsb() map[string]string {
+func getStatusUsb() (string, map[string]string) {
 	result := make(map[string]string)
 	r, error := regexp.Compile(" ([0-9a-f]{4}:[0-9a-fA-Z]{4}) (.*)")
 	if error != nil {
@@ -124,10 +145,10 @@ func getStatusUsb() map[string]string {
 		}
 	}
 	log.Println("Done.")
-	return result
+	return "", result
 }
 
-func getStatusEther(ouiMap map[string]string) map[string]string {
+func getStatusEther(ouiMap map[string]string) (string, map[string]string) {
 	nstr := "  \t\n"
 	//find out the default routing ip
 	var routeIP string
@@ -225,13 +246,10 @@ func getStatusEther(ouiMap map[string]string) map[string]string {
 			bundleListResolv = append(bundleListResolv, oui)
 		}
 	}
-	//fmt.Printf("%#v\n", bundleList)
-	//fmt.Printf("%#v\n", bundleListTypes)
-	//fmt.Printf("%#v\n", bundleListResolv)
 	fmt.Println("default route iface:" + ifnToEth[rountIfn])
 	fmt.Println("routeIp: " + routeIP)
 	log.Println("Finished.")
-	return result
+	return ifnToEth[rountIfn], result
 }
 
 func getOUIMap(filePath string) map[string]string {
@@ -266,9 +284,17 @@ func getOUIMap(filePath string) map[string]string {
 }
 
 func main() {
-
 	//lift https://blog.golang.org/go-maps-in-action
-	log.Println("Initializing...")
+	log.Printf("Initializing...\n")
+	config := Config{}
+	conffile, err := ioutil.ReadFile(iotconfigyaml)
+	if err != nil {
+		panic(err)
+	}
+	err = yaml.Unmarshal(conffile, &config)
+	if err != nil {
+		panic(err)
+	}
 
 	if fileExists(ouiFileLocal) {
 		//fmt.Println("Loading " + ouiFileLocal + ".")
@@ -278,25 +304,121 @@ func main() {
 
 	log.Println("Done.")
 	if !fileExists(ouiFileLocal) {
-
 		log.Panic("Unable to load " + ouiFileLocal)
 	}
 
 	ouiMap := getOUIMap(ouiFileLocal)
 	//fmt.Printf("%s\n", oui["test"])
-	/*lsusb := getStatusUsb()
-	bsonUsb, err := json.Marshal(lsusb)
+	_, maplsusb := getStatusUsb()
+
+	//https://stackoverflow.com/questions/31361375/how-to-check-if-a-monitor-is-plugged-in-linux
+	defroute, maplsether := getStatusEther(ouiMap)
+
+	iotBundle := IoTBundle{}
+	iotBundleOld := IoTBundle{}
+	seed := strings.NewReader(defroute + defroute) //TODO: better seeding func
+	uuid.SetRand(seed)
+	uid, err := uuid.NewRandom()
+	if err != nil {
+		panic(err)
+	}
+	//fmt.Printf("%#v %s\n", reflect.ValueOf(maplsether).MapKeys(), uid)
+	iotBundle.UUID = fmt.Sprintf("%s", uid)
+	iotBundle.ID = defroute
+	iotBundle.IDType = "mac"
+	for _, key := range reflect.ValueOf(maplsusb).MapKeys() {
+		iotBundle.BundleList = append(iotBundle.BundleList, fmt.Sprintf("%s", key))
+		iotBundle.BundleListType = append(iotBundle.BundleListType, "usb")
+		iotBundle.BundleListResolv = append(iotBundle.BundleListResolv, "") //TBD
+		iotBundle.BundleListDesc = append(iotBundle.BundleListDesc, maplsusb[fmt.Sprintf("%s", key)])
+
+		seed := strings.NewReader(fmt.Sprintf("%s", key) + fmt.Sprintf("%s", key)) //TODO: better seeding
+		uuid.SetRand(seed)
+		devuid, err := uuid.NewRandom()
+		var struid string
+		if err != nil {
+			panic(err)
+			struid = "00000000-0000-0000-0000-000000000000"
+		}
+		fmt.Printf("%s\n", fmt.Sprintf("%s", devuid))
+		struid = fmt.Sprintf("%s", devuid)
+		iotBundle.BundleListUUID = append(iotBundle.BundleListUUID, struid)
+	}
+	for _, key := range reflect.ValueOf(maplsether).MapKeys() {
+		iotBundle.BundleList = append(iotBundle.BundleList, fmt.Sprintf("%s", key))
+		iotBundle.BundleListType = append(iotBundle.BundleListType, "mac")
+		iotBundle.BundleListResolv = append(iotBundle.BundleListResolv, "") //TBD
+		iotBundle.BundleListDesc = append(iotBundle.BundleListDesc, maplsether[fmt.Sprintf("%s", key)])
+		seed := strings.NewReader(fmt.Sprintf("%s", key) + fmt.Sprintf("%s", key)) //TODO: better seeding
+		uuid.SetRand(seed)
+		devuid, err := uuid.NewRandom()
+		var struid string
+		if err != nil {
+			panic(err)
+			struid = "00000000-0000-0000-0000-000000000000"
+		}
+		struid = fmt.Sprintf("%s", devuid)
+		iotBundle.BundleListUUID = append(iotBundle.BundleListUUID, struid)
+	}
+
+	yamlfile, err := ioutil.ReadFile(iotstatusyaml)
+	if err != nil {
+		//iotBundle.BundleList = reflect.ValueOf(maplsether).MapKeys()
+		data, err := yaml.Marshal(iotBundle)
+		if err != nil {
+			panic(err)
+		}
+		f, err := os.Create(iotstatusyaml)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		f.Write(data)
+
+	} else {
+		err = yaml.Unmarshal(yamlfile, &iotBundleOld)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if iotBundleOld.UUID != "" {
+		found := false
+		for _, id := range iotBundle.BundleList {
+			for _, idOld := range iotBundleOld.BundleList {
+				if id == idOld {
+					found = true
+					break
+				}
+			}
+			if !found {
+				iotBundle.BundleListAdd = append(iotBundle.BundleListAdd, id)
+				iotBundle.HasChanged = true
+			}
+		}
+		for _, idOld := range iotBundleOld.BundleList {
+			for _, id := range iotBundle.BundleList {
+				if id == idOld {
+					found = true
+					break
+				}
+			}
+			if !found {
+				iotBundle.BundleListDel = append(iotBundle.BundleListDel, idOld)
+				iotBundle.HasChanged = true
+			}
+		}
+
+	}
+	iotBundle.LastSeen = time.Now().Format(time.RFC3339)
+	jsonOut, err := json.Marshal(iotBundle)
 	if err != nil {
 		fmt.Println("error:", err)
 	}
-	os.Stdout.Write(bsonUsb)
-	fmt.Println("")*/
-        //https://stackoverflow.com/questions/31361375/how-to-check-if-a-monitor-is-plugged-in-linux
-	lsether := getStatusEther(ouiMap)
-	bsonEther, err := json.Marshal(lsether)
-	if err != nil {
-		fmt.Println("error:", err)
-	}
-	os.Stdout.Write(bsonEther)
-	fmt.Println("")
+	resp, err := http.Post(config.URL,
+		"application/json",
+		strings.NewReader(string(jsonOut)))
+
+	fmt.Printf("%#v %s\n", resp, string(jsonOut))
+
 }
